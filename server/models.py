54e322d9ba010368c084641707eafa19451180be
Adedamola Ajasa <azeemajasa@gmail.com>
2023-11-15 22:03:05 +0100

    added models to app

---
+++ server/models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Products(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, Primary_Key = True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class Customers(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, Primary_Key = True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+
+class Reviews(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, Primary_Key = True)
+    customer_id = db.Column(db.Integer)
+    body = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
